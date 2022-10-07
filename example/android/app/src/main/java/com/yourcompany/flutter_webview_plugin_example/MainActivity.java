1c47217e07071ae1c260020f0e60c13f94a12dc0
Mattees <matej.snovak@gmail.com>
2022-10-07 13:50:18 +0200

    fix

--- example/android/app/src/main/java/com/yourcompany/flutter_webview_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/yourcompany/flutter_webview_plugin_example/MainActivity.java
@@ -1,8 +1,6 @@
 package com.yourcompany.flutter_webview_plugin_example;
 
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
 
 public class MainActivity extends FlutterActivity {
     
